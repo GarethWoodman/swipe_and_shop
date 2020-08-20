8caf34eb2e4e6c7670583a86c3f107065f5a8ea1
Andrew Hulme <andrewhulme1993@gmail.com>
2020-08-20 12:01:12 +0100

    Connects to MongoDB

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const MONGODB_URI =
+  "mongodb+srv://andrewhulme:MerryChristmas@swipeandshop.rtrxp.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+mongoose.connect(MONGODB_URI || "mongodb://localhost/swipe_and_shop", {
+  userNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose is connected!!!!");
+});
+
 // HTTP request logger
 app.use(morgan("tiny"));
 
