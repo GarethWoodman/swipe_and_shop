a456309f81a1d9217f4d428f07f590216b941479
Jed Stanton <jedstanton@hotmail.com>
2020-08-21 14:48:42 +0100

    React app form setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,49 @@
 import React from "react";
 
 class App extends React.Component {
-  state = {};
+  state = {
+    title: "",
+    body: "",
+  };
+
+  handleChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    console.log("state: ", this.state);
     return (
       <div>
         <h2>Welcome to my App</h2>
+        <form>
+          <div className="form-input">
+            <input
+              type="text"
+              name="title"
+              placeholder="Title"
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form-input">
+            <textarea
+              placeholder="Body"
+              name="body"
+              cols="30"
+              rows="10"
+              value={this.state.body}
+              onChange={this.handleChange}
+            ></textarea>
+          </div>
+
+          <button>Submit</button>
+        </form>
       </div>
     );
   }
