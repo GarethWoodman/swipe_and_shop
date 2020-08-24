6a42c1f22e1e0abcd1f9daf682bef3a3f630d69f
Jed Stanton <jedstanton@hotmail.com>
2020-08-24 14:41:14 +0100

    getBlogPost method added to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,24 @@ class App extends React.Component {
   state = {
     title: "",
     body: "",
+    posts: [],
+  };
+
+  componentDidMount = () => {
+    this.getBlogPost();
+  };
+
+  getBlogPost = () => {
+    axios
+      .get("/api")
+      .then((response) => {
+        const data = response.data;
+        this.setState({ posts: data });
+        console.log("Data has been received");
+      })
+      .catch(() => {
+        alert("Error retrieving data!!!");
+      });
   };
 
   handleChange = (event) => {
