e21a29774da58e228ef036eeb7397a8cb46cd524
Jed Stanton <jedstanton@hotmail.com>
2020-09-15 12:33:56 +0100

    Yes and No buttons added to iterate through items

--- client/src/components/buy.jsx
+++ client/src/components/buy.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 class Buy extends Component {
   state = {
     items: "",
+    itemNum: 0,
   };
 
   componentDidMount = () => {
@@ -20,15 +21,29 @@ class Buy extends Component {
       });
   };
 
+  nextItem = () => {
+    const num = this.state.itemNum;
+    if (num < this.state.items.length - 1) {
+      this.setState({ itemNum: num + 1 });
+    }
+  };
+
   render() {
     if (!this.state.items.length) return null;
     console.log(this.state.items[0].item_name);
     return (
       <div>
         <h1>Buy Page</h1>
-        <p>{this.state.items[0].item_name}</p>
-        <p>{this.state.items[0].description}</p>
-        <p>{this.state.items[0].price}</p>
+        <p>{this.state.items[this.state.itemNum].item_name}</p>
+        <p>{this.state.items[this.state.itemNum].description}</p>
+        <p>{this.state.items[this.state.itemNum].price}</p>
+        <img src={this.state.items[this.state.itemNum].picture} alt="pic"></img>
+
+        <button id="yesButton">Yes</button>
+
+        <button id="noButton" onClick={this.nextItem}>
+          No
+        </button>
       </div>
     );
   }
