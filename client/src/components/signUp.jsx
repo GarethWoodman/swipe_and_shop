360737b58839744e14ded9c076783084989a3291
Jed Stanton <jedstanton@hotmail.com>
2020-09-16 12:41:40 +0100

    Handle submit for signup created - not working yet

--- client/src/components/signUp.jsx
+++ client/src/components/signUp.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class SignUp extends Component {
   state = {
@@ -15,6 +16,33 @@ class SignUp extends Component {
     let attribute = event.target.id;
     let value = event.target.value;
     this.setState({ [attribute]: value });
+  };
+
+  handleSubmit = (event) => {
+    const payload = {
+      email: this.state.email,
+      password: this.state.password,
+      username: this.state.username,
+      real_name: this.state.real_name,
+      picture: this.state.picture,
+      to_buy: [1],
+      to_sell: [1],
+    };
+
+    axios({
+      url: "user/save",
+      method: "POST",
+      data: payload,
+    })
+      .then(() => {
+        console.log("Data has been sent to the server");
+        //this.resetUserInputs();
+      })
+      .catch(() => {
+        console.log("Internal server error");
+      });
+
+    event.preventDefault();
   };
 
   render() {
@@ -41,7 +69,7 @@ class SignUp extends Component {
           <p>picture</p>
           <input id="picture" type="text" onChange={this.myChangeHandler} />
           <p></p>
-          <input type="submit" value="Submit" />
+          <input type="submit" value="Submit" onSubmit={this.handleSumbit} />
         </form>
       </div>
     );
