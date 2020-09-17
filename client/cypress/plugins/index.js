f20ef642b7afcf92a7e27a4fd2035becad0df276
Andrew Hulme <andrewhulme1993@gmail.com>
2020-09-17 10:53:07 +0100

    Adds cypress helper functions for test database

--- client/cypress/plugins/index.js
+++ client/cypress/plugins/index.js
@@ -12,10 +12,54 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const mongoose = require("mongoose");
+const User = require("../../../models/user");
+const Item = require("../../../models/item");
+
 /**
  * @type {Cypress.PluginConfig}
  */
-module.exports = (on, config) => {
+module.exports = (on) => {
   // `on` is used to hook into various events Cypress emits
+  on("task", {
+    addItem(item) {
+      return new Promise((resolve) => {
+        mongoose.connect("mongodb://localhost/swipe_and_shop_test", (err) => {
+          newItem = new Item(item);
+          newItem.save((err) => {
+            resolve("done");
+          });
+        });
+      });
+    },
+    addUser(user) {
+      return new Promise((resolve) => {
+        mongoose.connect("mongodb://localhost/swipe_and_shop_test", (err) => {
+          newUser = new User(user);
+          newUser.save((err) => {
+            resolve("done");
+          });
+        });
+      });
+    },
+    dropItems() {
+      return new Promise((resolve) => {
+        mongoose.connect("mongodb://localhost/swipe_and_shop_test", (err) => {
+          mongoose.connection.collections.items.drop((err) => {
+            resolve("done");
+          });
+        });
+      });
+    },
+    dropUsers() {
+      return new Promise((resolve) => {
+        mongoose.connect("mongodb://localhost/swipe_and_shop_test", (err) => {
+          mongoose.connection.collections.users.drop((err) => {
+            resolve("done");
+          });
+        });
+      });
+    },
+  });
   // `config` is the resolved Cypress config
-}
+};
