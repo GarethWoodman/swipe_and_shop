096e528f7e78038ab39387e25c796532d9d53df5
Andrew Hulme <andrewhulme1993@gmail.com>
2020-09-18 10:41:41 +0100

    Cypress tests go onto port 3000

--- client/cypress/integration/homepage.spec.js
+++ client/cypress/integration/homepage.spec.js
@@ -1,8 +1,16 @@
 describe("Homepage", () => {
   beforeEach(() => {
-    cy.task('dropItems');
-    cy.task('addItem', {"user_id":"69","item_name":"Makers_test","description":"Makers Course","expiry_date":"6 months I reckon","picture":"https://coursereport-s3-production.global.ssl.fastly.net/rich/rich_files/rich_files/5940/s300/screen-shot-2020-03-30-at-14-28-10.png","price":8000.00});
-    cy.visit("http://localhost:3030");
+    cy.task("dropItems");
+    cy.task("addItem", {
+      user_id: "69",
+      item_name: "Makers_test",
+      description: "Makers Course",
+      expiry_date: "6 months I reckon",
+      picture:
+        "https://coursereport-s3-production.global.ssl.fastly.net/rich/rich_files/rich_files/5940/s300/screen-shot-2020-03-30-at-14-28-10.png",
+      price: 8000.0,
+    });
+    cy.visit("http://localhost:3000");
   });
 
   it("Displays the message in the list", () => {
