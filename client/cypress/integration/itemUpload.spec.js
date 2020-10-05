04c02c4c4474f86fd194de5f1e252f777bd5c215
Andrew Hulme <andrewhulme1993@gmail.com>
2020-10-05 11:16:03 +0100

    Adds test for uploading an item

--- client/cypress/integration/itemUpload.spec.js
+++ client/cypress/integration/itemUpload.spec.js
@@ -2,15 +2,15 @@ describe("Sell Items Page", () => {
   beforeEach(() => {
     cy.task("dropUsers");
     cy.task("dropItems");
-    cy.task("addItem", {
-      user_id: "69",
-      item_name: "Makers_test",
-      description: "Makers Course",
-      expiry_date: "6 months I reckon",
-      picture:
-        "https://coursereport-s3-production.global.ssl.fastly.net/rich/rich_files/rich_files/5940/s300/screen-shot-2020-03-30-at-14-28-10.png",
-      price: 8000.0,
-    });
+    // cy.task("addItem", {
+    //   user_id: "69",
+    //   item_name: "Makers_test",
+    //   description: "Makers Course",
+    //   expiry_date: "6 months I reckon",
+    //   picture:
+    //     "https://coursereport-s3-production.global.ssl.fastly.net/rich/rich_files/rich_files/5940/s300/screen-shot-2020-03-30-at-14-28-10.png",
+    //   price: 8000.0,
+    // });
 
     cy.task("addUser", {
       email: "jag@jag.com",
@@ -33,16 +33,24 @@ describe("Sell Items Page", () => {
   });
 
   it("has item upload form", () => {
-    // const imageAddress =
-    //   "https://static01.nyt.com/images/2019/04/02/science/28SCI-ZIMMER1/28SCI-ZIMMER1-videoSixteenByNineJumbo1600.jpg";
-    // cy.get("#itemUploadName").type("Frog").should("have.value", "Frog");
-    // cy.get("#itemUploadDescription")
-    //   .type("Croaky")
-    //   .should("have.value", "Croaky");
-    // cy.get("#itemUploadPrice").type("10").should("have.value", "10");
-    // cy.get("#itemUploadExpiry").type("1 year").should("have.value", "1 year");
-    // cy.get("#itemUploadPicture")
-    //   .type(imageAdress)
-    //   .should("have.value", imageAddress);
+    const imageAddress =
+      "https://static01.nyt.com/images/2019/04/02/science/28SCI-ZIMMER1/28SCI-ZIMMER1-videoSixteenByNineJumbo1600.jpg";
+    cy.get("#itemUploadName").type("Frog").should("have.value", "Frog");
+    cy.get("#itemUploadDescription")
+      .type("Croaky")
+      .should("have.value", "Croaky");
+    cy.get("#itemUploadPrice").type("10").should("have.value", "10");
+    cy.get("#itemUploadExpiry").type("1 year").should("have.value", "1 year");
+    cy.get("#itemUploadPicture")
+      .type(imageAdress)
+      .should("have.value", imageAddress);
+    cy.get("#itemSubmit").click();
+
+    cy.contains("Buy Page");
+
+    cy.get("#itemName").contains("Frog");
+    cy.get("#itemDescription").contains("Croaky");
+    cy.get("#itemPrice").contains("10");
+    cy.get("#itemPicture");
   });
 });
